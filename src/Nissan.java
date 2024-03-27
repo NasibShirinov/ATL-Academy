88e228f15a51d7b41fd99debd193d550b4a315a8
nasib-514 <basqalli14@gmail.com>
2024-03-27 14:03:06 +0400

    Its homework2 on 25.03.2024ghkhgk

---
+++ src/Nissan.java
@@ -0,0 +1,7 @@
+public class Nissan {
+    static int speed = 180;
+
+    public static void main(String[] args) {
+        System.out.println(speed);
+    }
+}
---
+++ src/myPackage/Cat.java
@@ -0,0 +1,7 @@
+package myPackage;
+
+public class Cat {
+    public static void main(String[] args) {
+        System.out.println("This is a Cat");
+    }
+}
